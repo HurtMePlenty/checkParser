e229d98862f1efbc77bb318f712f244ec5913050
asemenov <semenov@goodwix.ru>
2015-03-24 20:23:41 +0300

    change

--- src/main/java/imageProcessor/EntryPoint.java
+++ src/main/java/imageProcessor/EntryPoint.java
@@ -1,6 +1,11 @@
 package imageProcessor;
 
+import magick.ImageInfo;
+import magick.ImageMagick;
+import magick.MagickImage;
 import net.sourceforge.tess4j.Tesseract;
+import org.bytedeco.javacv.CanvasFrame;
+import org.bytedeco.javacv.JavaCV;
 
 import java.io.File;
 
@@ -8,14 +13,23 @@ public class EntryPoint
 {
     public static void main(String[] args)
     {
-        //File image = new File("data\\receipt_1424763681-14_723240.jpg");
-        //File image = new File("data\\r14238502408_681231.jpg");
-        File image = new File("data\\test1.jpg");
-        Tesseract tesseract = Tesseract.getInstance();
         try
         {
-           String result = tesseract.doOCR(image);
-            System.out.println(result);
+            //File image = new File("data\\receipt_1424763681-14_723240.jpg");
+            //File image = new File("data\\r14238502408_681231.jpg");
+            String fileName = "data\\qq.png";
+            File imageFile = new File(fileName);
+            ImageInfo imageInfo = new ImageInfo(fileName);
+            MagickImage image = new MagickImage(imageInfo); //load image
+
+            //CanvasFrame cf = new CanvasFrame();
+
+
+            /*Tesseract tesseract = Tesseract.getInstance();
+
+            String result = tesseract.doOCR(image);
+            String resultString = result.replaceAll("[^\\x00-\\x7F]", "").replace("|", "");
+            System.out.println(resultString);   */
         }
         catch (Exception e)
         {
